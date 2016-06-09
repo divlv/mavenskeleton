961c001d442f828be3946b520a4ea539a3ee69e6
worldwar2 <worldwar2@div.lv>
2016-06-09 13:48:55 +0300

    Initial commit

---
+++ src/main/java/lv/div/project/Main.java
@@ -0,0 +1,11 @@
+package lv.div.project;
+
+import java.util.Date;
+/**
+ * Main starting point.
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println( "App is working! Current time is " + new Date());
+		}
+}
